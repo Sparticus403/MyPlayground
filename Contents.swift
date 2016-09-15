eae0e3006372f56e8bd30483a93d0756013db080
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-15 09:55:20 -0600

    Created repo for my first playground project.

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var number = 123456
+print(number + 12)
+print("\(number) " + str)
+let meaningOfLife = 42
+print(meaningOfLife)
+print(meaningOfLife + number)
